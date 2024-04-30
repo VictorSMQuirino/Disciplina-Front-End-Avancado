ac447f32567bcf4b051bf7cf4db1501b5845ee2a
Victor Quirino <victorsouzaq@gmail.com>
2024-04-30 17:30:16 -0300

    Finalizada

--- FEA-P010/src/app/components/carrinho-compras/carrinho-compras.component.ts
+++ FEA-P010/src/app/components/carrinho-compras/carrinho-compras.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, computed, signal } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import { Item } from '../../models/item.model';
 import { ShoppingService } from '../../services/shopping.service';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-carrinho-compras',
   standalone: true,
-  imports: [MatCardModule, CommonModule, MatIconModule],
+  imports: [MatCardModule, CommonModule, MatIconModule, MatButtonModule],
   templateUrl: './carrinho-compras.component.html',
   styleUrl: './carrinho-compras.component.css'
 })
@@ -17,11 +18,19 @@ export class CarrinhoComprasComponent implements OnInit {
 
   constructor(private shoppingService: ShoppingService) { }
 
+  listaItens = signal(this.itensNoCarrinho);
+
   ngOnInit(): void {
     this.itensNoCarrinho = this.shoppingService.getItensNoCarrinho();
+    this.listaItens.set(this.itensNoCarrinho);
   }
 
-  removerItemDoCarrinho(item: Item) { 
+  removerItemDoCarrinho(item: Item) {
     this.shoppingService.removerItemDoCarrinho(item);
+    this.listaItens.set(this.shoppingService.getItensNoCarrinho()); 
   }
+
+  precoTotal = computed(() => {
+    return this.listaItens().reduce((acc, item) => acc + (item.price), 0);
+  });
 }
