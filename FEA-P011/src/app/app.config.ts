523d2107c38b6a4d71960bea7134390b68b1f890
Victor Quirino <victorsouzaq@gmail.com>
2024-05-07 11:04:40 -0300

    feat - componente de exibir tarefas

--- FEA-P011/src/app/app.config.ts
+++ FEA-P011/src/app/app.config.ts
@@ -3,7 +3,8 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
+import { tarefasReducer } from './store/tarefa.reducer';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideStore()]
+  providers: [provideRouter(routes), provideStore({tarefas: tarefasReducer})]
 };
---
+++ FEA-P011/src/app/components/show-tarefas/show-tarefas.component.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Tarefa } from '../../../models/tarefa.model';
+import { TarefaState } from '../../store/tarefa.reducer';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { selectorSelecionaTarefa } from '../../store/tarefa.seletors';
+import { removerTarefa } from '../../store/tarefa.action';
+
+@Component({
+  selector: 'app-show-tarefas',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './show-tarefas.component.html',
+  styleUrl: './show-tarefas.component.css'
+})
+export class ShowTarefasComponent implements OnInit {
+  tarefas: Tarefa[] = [{id: '1', descricao: 'Descrição 1'},];
+  tasks$!: Observable<TarefaState>;
+
+  constructor(private store: Store<{tarefas: TarefaState}>) { }
+
+  ngOnInit(): void {
+    this.tasks$ = this.store.select(selectorSelecionaTarefa);
+    this.tasks$.subscribe((task) => {
+      this.tarefas = task.tarefas;
+    });
+  }
+
+  removeTarefa(id: string) {
+    this.store.dispatch(removerTarefa({id: id}));
+  }
+}
